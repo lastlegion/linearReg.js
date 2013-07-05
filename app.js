1c047c8e4d55e647afd43c06ad447d1a63677d74
lastlegion <lastlegion@gmail.com>
2013-07-06 01:14:13 +0530

    Added function to prepare training data. Ready for deployment.

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ var fs = require('fs'),
     sylvester = require('sylvester'),
     Matrix = sylvester.Matrix,
     Vector = sylvester.Vector,
-    linearReg = require('./linearReg');
+    linearReg = require('./lib/linearReg');
 
-var X = [] //features
-var y = [] //labels
-var theta = [0,0];
 
-function parseInput(fileName, callback){
-    fs.readFile('ex1data1.txt', 'utf8', function(err,data){
+function parseInput(fileName, X, y, callback){
+    fs.readFile(fileName, 'utf8', function(err,data){
         lines = data.split("\n");
         for(var i=0; i<lines.length; i++){
             var line = lines[i].split(",");
@@ -21,21 +18,26 @@ function parseInput(fileName, callback){
         callback();
     });
 }
+
 var main = function(){
     var input_file = process.argv[2];
     if(input_file === undefined){
         console.log("Usage: node app.js <filename>");
+        return;
     }
-    parseInput(input_file, function(){
-        //Append 1s to X
-        for(var i=0; i<X.length; i++){
-            X[i] = [1, X[i]];
-        }
-        //Vectorize inputs
-        var X_mat = Matrix.create(X);
-        var y_vec = Vector.create(y);
-        var theta_vec = Vector.create(theta);     
+    var X = [] //features
+    var y = [] //labels
+    var theta = [0,0];  //linear regression parameters
+
+    parseInput(input_file, X, y, function(){       
+               
+        //Vectorize training data
+        var trainingData = linearReg.prepareTrainingData(X,y,theta);      
+        X_mat = trainingData["X_mat"];
+        y_vec = trainingData["y_vec"];
+        theta_vec = trainingData["theta_vec"];
         
+        //Configurations for gradient descent
         var iterations = 2500;
         var alpha = 0.01;
 
